934516317b41435927df94e651568ac92f2f4138
evanmico <evanmico222@gmail.com>
2025-01-17 17:54:46 -0500

    fixed date parsing being messed up for projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -14,6 +14,7 @@ function InfoSection({headingText, bodyText, extra}){
 }
 
 function Project({title,date,description}) {
+    const dateObj = new Date(date);
     return (
         <section className="shadow-lg bg-txt-lg/5 border-solid space-y-4 border-4 border-txt-lg rounded-xl p-3">
             <div className="flex place-content-between border-b-4 pb-2 border-txt-lg">
@@ -21,7 +22,7 @@ function Project({title,date,description}) {
                     {title}
                 </h3>
                 <h3 className="text-accent-dark text-3xl font-bold">
-                    {date.getDay()}/{date.getMonth()}/{date.getFullYear()}
+                    {dateObj.getDate()}/{dateObj.getMonth()+1}/{dateObj.getFullYear()} {console.log(dateObj)}
                 </h3>
             </div>
             <p className="text-txt-sm text-2xl">
@@ -32,10 +33,10 @@ function Project({title,date,description}) {
 }
 
 function ProjectList({projects}) {
-    const projectSet = projects.map(({title,date,description},idx)=>(<Project key={idx} title={title} date={date} description={description}/>));
+    //const projectSet = projects.map(({title,date,description, link},idx)=>(<Project key={idx} title={title} date={date} description={description} link={link}/>));
     return (
         <section className="space-y-6">
-            {projectSet}
+            {projects.map(({title,date,description, link},idx)=>(<Project key={idx} title={title} date={date} description={description} link={link}/>))}
         </section>
     )
 }
@@ -44,35 +45,35 @@ function Projects() {
     const projects = [
         {
             "title":"OCR for Mariokart Tournament Score Tracking",
-            "date": new Date(2024,11,27),
+            "date":"2024-11-27",
             "description":"A first attempt at using an open source OCR model called PaddleOCR to scrape leaderboard scores from screenshots of finished races in Mariokart 8 Deluxe.",
             "link":"https://github.com/evanmico/mk8dx-raceTable-PaddleOCR"
         },
         {
             "title":"Game Song Web Scraper",
-            "date": new Date(2024,10,22),
+            "date":"2024-10-22",
             "description":"A project purely because a site wanted to charge me if I wanted to download an entire album with one click, so I made a scraper to download each song in it individually automatically which was free."
         },
         {
             "title":"Rummikub Web Game at Shellhacks 2024",
-            "date": new Date(2024,3,6),
+            "date":"2024-10-01",
             "description":"A project attempted at a hackathon to digitize a game I love called Rummikub.",
             "link":"https://github.com/evanmico/shellhacks2024-rumikubGame_frontend"
         },
         {
-            "title":"Physical Healthcare office Client Managment System",
-            "date": new Date(2024,4,3),
-            "description":"A web app that acts as a portal for clients to schedule appointments with a physical therapist."
-        },
-        {
             "title":"MK Leaderboard Discord Bot",
-            "date": new Date(2024,2,20),
+            "date": "2024-02-20",
             "description":"A bot that keeps a live leaderboard of the current timetrial top times in a designated thread.",
             "link":"https://github.com/evanmico/mk-leaderboard-discord-bot"
         },
         {
+            "title":"Physical Healthcare Office Client Managment System",
+            "date":"2024-02-17",
+            "description":"A web app that acts as a portal for clients to schedule appointments with a physical therapist."
+        },
+        {
             "title":"Volunte at Knighthacks 2023",
-            "date": new Date(2023,10,8),
+            "date": "2023-10-08",
             "description":"A webapp built during a hackathon with the goal of helping volunteering organizations find volunteers and volunteers to find volunteering work.",
             "link":"https://github.com/webbgamers/volunte"
         }
